4d5befebf62401dd86b10e43e7c3c092eaca219b
oleksandr.loginov <oleksandr.loginov@betlab.com>
2018-03-25 19:22:17 +0300

    add simple caching on server

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router()
+var cache = require('memory-cache');
 
 var Twitter = require('twitter');
 var client = new Twitter({
@@ -15,13 +16,21 @@ router.get('/tweets', function(req, res, next) {
   if (!q) {
     res.json({ statuses: [] });
   } else {
-    client.get('search/tweets', { q: q }, function(error, tweets, response) {
-      if (!error) {
-        res.json(tweets);
-      } else {
-        res.status(500).json({ error: error });
-      }
-    });
+    var key = `tweets_${q}`;
+    var cached = cache.get(key);
+
+    if (cached) {
+      res.json(cached);
+    } else {
+      client.get('search/tweets', { q: q }, function(error, data, response) {
+        if (!error) {
+          res.json(data);
+          cache.put(key, data, 10 /* sec */ * 1000);
+        } else {
+          res.status(500).json({ error: error });
+        }
+      });
+    }
   }
 });
 
